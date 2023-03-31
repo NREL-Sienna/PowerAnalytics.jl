85a01300cac07ba428d5fa83b6d40fa4239ac73e
Documenter.jl <documenter@juliadocs.github.io>
2023-03-31 05:58:53 +0000

    build based on e0c4147

---
+++ v0.3.2/PowerAnalytics/3.0_getting_started.jl
@@ -0,0 +1,9 @@
+# # Getting Started with PowerAnalytics
+
+# Start by creating a results object using [PowerSimulations](http://github.com/nrel-siip/PowerSimulations.jl.git)
+
+# Set up PowerAnalytics:
+# ```julia
+# import PowerAnalytics
+# const PA = PowerAnalytics
+# ```
