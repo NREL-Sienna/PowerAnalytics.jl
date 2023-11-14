a1f49b65ea44d8d8441f1b5bdab46daec25ca9c8
GabrielKS <23368820+GabrielKS@users.noreply.github.com>
2023-11-14 15:50:24 -0700

    Implement ComponentEntity
    This is the first commit in a major PowerAnalytics redesign.

--- src/PowerAnalytics.jl
+++ src/PowerAnalytics.jl
@@ -7,6 +7,10 @@ export get_service_data
 export categorize_data
 export no_datetime
 
+export Entity, EntityElement, EntitySet
+export subtype_to_string, component_to_qualified_string
+export make_entity, default_name, get_name
+
 #I/O Imports
 import Dates
 import TimeSeries
@@ -14,7 +18,9 @@ import DataFrames
 import YAML
 import DataStructures: OrderedDict, SortedDict
 import PowerSystems
+import PowerSystems: Component, get_component, get_components
 import InfrastructureSystems
+import InfrastructureSystems: get_name
 import PowerSimulations
 import InteractiveUtils
 
@@ -25,6 +31,7 @@ const PSI = PowerSimulations
 include("definitions.jl")
 include("get_data.jl")
 include("fuel_results.jl")
+include("entities.jl")
 
 greet() = print("Hello World!")
 
