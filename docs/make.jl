8fd5c31a962cc476795a7e5cdd32bf57b0337dba
annacasavant <anca7065@colorado.edu>
2025-02-05 15:51:10 -0700

    updated documentation to follow diataxis and updated to Documenter 1.0

--- docs/make.jl
+++ docs/make.jl
@@ -1,53 +1,53 @@
 using Documenter
 using PowerAnalytics
-using Literate
+import DataStructures: OrderedDict
 
-folders = Dict()
 
-for (name, folder) in folders
-    for file in folder
-        outputdir = joinpath(pwd(), "docs/src/howto")
-        inputfile = joinpath(pwd(), "docs/src/$name/$file")
-        Literate.markdown(inputfile, outputdir)
-    end
-end
-if isfile("docs/src/howto/.DS_Store.md")
-    rm("docs/src/howto/.DS_Store.md")
-end
-
-makedocs(
-    sitename = "PowerAnalytics.jl",
-    format = Documenter.HTML(
-        mathengine = Documenter.MathJax(),
-        prettyurls = get(ENV, "CI", nothing) == "true",
-    ),
-    modules = [PowerAnalytics],
-    authors = "Clayton Barrows",
-    pages = Any[
-        "Introduction" => "index.md",
-        #"Quick Start Guide" => "qs_guide.md",
-        #"Logging" => "man/logging.md",
-        # "Operation Model" => "man/op_problem.md",
-        # "How To" => Any[
-        #     "Set Up Plots" => "howto/3.0_set_up_plots.md",
-        #     "Make Stack Plots" => "howto/3.1_make_stack_plots.md",
-        #     "Make Bar Plots" => "howto/3.2_make_bar_plots.md",
-        #     "Make Fuel Plots" => "howto/3.3_make_fuel_plots.md",
-        #     "Make Forecast Plots" => "howto/3.4_make_forecast_plots.md",
-        #     "Plot Fewer Variables" => "howto/3.5_plot_fewer_variables.md",
-        #     "Plot Multiple Results" => "howto/3.6_plot_multiple_results.md",
-        # ],
-        #"Simulation Recorder" => "man/simulation_recorder.md",
-        #"Model References" => Any["Hydro Models" => "ref/hydro.md"],
-        "API" => Any["PowerAnalytics" => "api/PowerAnalytics.md"],
-    ],
+pages = OrderedDict(
+    "Welcome Page" => "index.md",
+    "Tutorials" => Any["stub" => "tutorials/stub.md"],
+    "How to..." => Any["stub" => "how_to_guides/stub.md"],
+    "Explanation" => Any["stub" => "explanation/stub.md"],
+    "Reference" => Any[ 
+        "Public API" => "reference/public.md",
+        "Developers" => ["Developer Guidelines" => "reference/developer_guidelines.md",
+        "Internals" => "reference/internal.md"]]
+     
 )
 
+
+
+
+makedocs(
+    modules = [PowerAnalytics],
+    format = Documenter.HTML(
+        prettyurls = haskey(ENV, "GITHUB_ACTIONS"),
+        size_threshold = nothing,),
+    sitename = "PowerAnalytics.jl",
+    authors = "Clayton Barrows",
+    pages = Any[p for p in pages],
+    draft = false,
+)
+
+
 deploydocs(
-    repo="github.com/NREL-Sienna/PowerAnalytics.jl.git",
+    repo="github.com/NREL-Sienna/PowerAnalytics.jl",
     target="build",
     branch="gh-pages",
     devbranch="main",
     devurl="dev",
+    push_preview=true,
     versions=["stable" => "v^", "v#.#"],
 )
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ docs/src/reference/internal.md
@@ -0,0 +1,6 @@
+## Internal
+
+```@autodocs
+Modules = [PowerAnalytics]
+Public = false
+```
